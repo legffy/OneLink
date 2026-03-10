472e3909a318bb5a6cda6551a7efada1af3fd568
Kevin Z <rpizhuk5@gmail.com>
2026-03-10 17:28:41 -0400

    fixed typescript

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,38 +1,34 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
+    return (
+        <>
+            <header className="navbar">
+                <div className="navLeft">
+                    <h1 id="homeLink">OneLink</h1>
+                </div>
 
-<body>
-<header className="navbar">
-    <div className="navLeft">
-        <h1 id="homeLink">OneLink</h1>
-    </div>
-    <div className="navRight">
-        <a href="#" id="buildingLink">Buildings</a>
-        <a href="#" id="mapLink">Map</a>
-        <a href="#" id="announcementsLink">Announcements</a>
-    </div>
-</header>
+                <div className="navRight">
+                    <a id="buildingLink">Buildings</a>
+                    <a id="mapLink">Map</a>
+                    <a id="announcementsLink">Announcements</a>
+                </div>
+            </header>
 
-<main id="app">
-    <section id="buildingView" className="view">
-        <h2>Campus Buildings</h2>
-        <div id="buildingList"></div>
-    </section>
+            <main id="app">
+                <section id="buildingView" className="view">
+                    <h2>Campus Buildings</h2>
+                    <div id="buildingList"></div>
+                </section>
 
-    <section id="mapView" className="view hidden">
-        <h2>Campus Map</h2>
-        <p>Map will go here later.</p>
-    </section>
+                <section id="mapView" className="view hidden">
+                    <h2>Campus Map</h2>
+                    <p>Map will go here later.</p>
+                </section>
 
-    <section id="announcementView" className="view hidden">
-        <h2>Announcements</h2>
-        <p>Emails will go here later.</p>
-    </section>
-</main>
-</body>
-
-
-  );
+                <section id="announcementView" className="view hidden">
+                    <h2>Announcements</h2>
+                    <p>Emails will go here later.</p>
+                </section>
+            </main>
+        </>
+    );
 }
