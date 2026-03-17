f1b8352c418865b809c3eb000f7151ba22b9aa3b
legffy <mrrst3@gmail.com>
2026-03-17 16:27:32 -0400

    feat normalize campuses

--- server/integrations/ems/sync.py
+++ server/integrations/ems/sync.py
@@ -4,13 +4,21 @@ from integrations.ems.client import getCurrentEvents, getGivenDayEvents, browse_
 import requests
 from core.models import Building, Reservation, Room
 
+def assign_campus() -> str:
+    buildings = Building.objects.all()
+    for building in buildings:
+        if "ec" in building.name.lower():
+            Building.objects.filter(external_building_id=building.external_building_id).update(campus="East Campus Athletic Village")
+        else:
+            Building.objects.filter(external_building_id=building.external_building_id).update(campus="Main Campus")
 
-def insert_current_events():
+def insert_current_events() -> None:
     current_time = datetime.now()
     events = getGivenDayEvents(current_time.day, current_time.month, current_time.year)
     buildings = events["buildings"]
     reservations = events["reservations"]
     rooms = events["rooms"]
+    print("Buildings: ", buildings)
 
     for building in buildings:
         Building.objects.update_or_create(
