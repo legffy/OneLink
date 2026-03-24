9d33fa922f216fee1df511db81f8bce72334a4f8
EthanG2006 <eguo5225@gmail.com>
2026-03-24 16:43:23 -0400

    added building images

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   const [open, setOpen] = useState(false);
 
   const buildings = [
-  { name: "Darrin Communications Center", slug: "darrin-communications-center" , seats: 550, image: "bot.png", abbreviation: "DCC"},
-  { name: "Low Center", slug: "low-center", seats: 550, image: "bot.png", abbreviation: "LOW"},
-  { name: "Russell Sage Laboratory", slug: "russell-sage-laboratory", seats: 550, image: "bot.png", abbreviation: "SAGE LABORATORY" },
-  { name: "Pittsburgh Building", slug: "pittsburgh-building", seats: 550, image: "bot.png", abbreviation: "PITTSBURGH"},
-  { name: "Folsom Library", slug: "folsom-library", seats: 900, image: "bot.png", abbreviation: "FOLSOM"}
+  { name: "Darrin Communications Center", slug: "darrin-communications-center" , seats: 550, image: "darrin.png", abbreviation: "DCC"},
+  { name: "Low Center", slug: "low-center", seats: 550, image: "low.jpg", abbreviation: "LOW"},
+  { name: "Russell Sage Laboratory", slug: "russell-sage-laboratory", seats: 550, image: "sage_lab.jpg", abbreviation: "SAGE LABORATORY" },
+  { name: "Pittsburgh Building", slug: "pittsburgh-building", seats: 550, image: "pittsburgh.jpg", abbreviation: "PITTSBURGH"},
+  { name: "Folsom Library", slug: "folsom-library", seats: 900, image: "folsom_library.jpg", abbreviation: "FOLSOM"}
   ];
 
 
