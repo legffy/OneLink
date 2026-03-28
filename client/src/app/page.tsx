6548e360985e1e0161b96fc975d703bd51f47a76
legffy <mrrst3@gmail.com>
2026-03-27 20:23:13 -0400

    feat: testing getting building to frontend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,44 +2,18 @@
 
 import BuildingCard from "./components/buildingCard";
 import { buildings } from "./data/buildings";
-
+import { useState, useEffect } from "react";
 export default function Home() {
-  const [open, setOpen] = useState(false);
-
-  const buildings = [
-  { name: "Darrin Communications Center", slug: "darrin-communications-center" , seats: 550, image: "bot.png", abbreviation: "DCC"},
-  { name: "Low Center", slug: "low-center", seats: 550, image: "bot.png", abbreviation: "LOW"},
-  { name: "Russell Sage Laboratory", slug: "russell-sage-laboratory", seats: 550, image: "bot.png", abbreviation: "SAGE LABORATORY" },
-  { name: "Pittsburgh Building", slug: "pittsburgh-building", seats: 550, image: "bot.png", abbreviation: "PITTSBURGH"},
-  { name: "Folsom Library", slug: "folsom-library", seats: 900, image: "bot.png", abbreviation: "FOLSOM"}
-  ];
-  const [apibuildings, setApiBuildings] = useState({});
-  const getBuildingInfo = async () => {
-    try{
-      const res = await fetch("http://127.0.0.1:8000/api/buildings/");
-      const data = await res.json();
-      setApiBuildings(data);
-      console.log("Feteched building data:",data);
-    } catch (error){
-      console.error("Error fetching tree data:", error);
-    }
-  }
-  const [treeData, setTreeData] = useState<Node[]>([]);
-  const fetchTreeData = async () => {
-    try {
-      const res = await fetch("http://localhost:8000/all_instruments_by_scale");
-      const data = await res.json();
-      let newTreeData: Node[] | undefined = createTree(data);
-      setTreeData(newTreeData || [{ name: "No data", children: undefined }]);
-      console.log("Fetched tree data:", data);
-    } catch (error) {
-      console.error("Error fetching tree data:", error);
-    }
-  };
-  useEffect(() => {
-    fetchTreeData();
-  }, []);
-
+  const [apibuildings, setApiBuildings] = useState({}); 
+  const getBuildingInfo = async () => { 
+    try{ const res = await fetch("http://127.0.0.1:8000/api/buildings/"); 
+      const data = await res.json(); setApiBuildings(data); 
+      console.log("Feteched building data:",data); } 
+      catch (error){ 
+        console.error("Error fetching tree data:", error); 
+      } 
+    } 
+    useEffect(() => { getBuildingInfo() },[])
   return (
     <main id="app" className="min-h-screen bg-[radial-gradient(circle_at_top,rgba(200,16,46,0.12),transparent_36%),linear-gradient(180deg,#fff8f5_0%,#ffffff_48%,#f5f7fb_100%)] px-6 py-10">
       <section id="buildingView" className="view mx-auto max-w-6xl">
