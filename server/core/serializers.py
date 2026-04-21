0a999b3ec3883c07e33f66f3fe80a84f353ae46b
Kevin Z <rpizhuk5@gmail.com>
2026-04-21 16:53:16 -0400

    structural cleanup

--- server/core/serializers.py
+++ server/core/serializers.py
@@ -1,18 +1,25 @@
 from rest_framework import serializers
-from .models import Building, Reservation, Room, Group
+from .models import Building, Group, Reservation, Room
+
 
 class BuildingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Building
         fields = "__all__"
+
+
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = "__all__"
+
+
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = "__all__"
+
+
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
