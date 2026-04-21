a3011a1b078e7217f8a1fe6d678eea147f4e77f2
Kevin Z <rpizhuk5@gmail.com>
2026-04-21 16:52:30 -0400

    changed navbar

--- client/src/app/components/navBar.tsx
+++ client/src/app/components/navBar.tsx
@@ -7,6 +7,7 @@ import { buildings } from "../data/buildings";
 export default function NavBar() {
   const [open, setOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const menuId = "building-menu";
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -36,17 +37,21 @@ export default function NavBar() {
       <div className="navRight">
         <div className="dropdown" ref={dropdownRef}>
           <button
+            type="button"
             id="buildingLink"
             onClick={() => setOpen(!open)}
             className="dropdownButton"
+            aria-expanded={open}
+            aria-controls={menuId}
+            aria-haspopup="menu"
           >
             Buildings &#9662;
           </button>
           {open && (
-            <ul className="dropdownMenu">
+            <ul id={menuId} className="dropdownMenu" role="menu" aria-labelledby="buildingLink">
               {buildings.map((building) => (
-                <li key={building.slug} className="dropdownItem">
-                  <Link href={`/buildings/${building.slug}`} onClick={() => setOpen(false)}>
+                <li key={building.slug} className="dropdownItem" role="none">
+                  <Link href={`/buildings/${building.slug}`} onClick={() => setOpen(false)} role="menuitem">
                     {building.name}
                   </Link>
                 </li>
